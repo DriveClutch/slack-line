260535cb71b40b117b45751481cfdc97900277ba
GrantMStevens <grant.stevens007@gmail.com>
2016-04-22 14:20:16 -0400

    fixing channels bug

--- app/config.js
+++ app/config.js
@@ -1,22 +1,4 @@
 export var config = {
-    //channels: window.slackClientChannels
-    channels: [
-        {
-            name: 'c_i_a_',
-            displayName: 'CIA'
-        },
-        {
-            name: 'member_chats',
-            displayName: 'Member Chats'
-        },
-        {
-            name: 'flip_ratings_stream',
-            displayName: 'Flip Ratings'
-        },
-        {
-            name: 'nlp_routing',
-            displayName: 'NLP'
-        }
-    ]
-}
+    channels: window.slackClientChannels
+};
 
